fe91824826d819a26e6a2b19c5a0649efa1ceb7f
Eren GUN <erengun4@gmail.com>
2023-12-03 11:43:35 +0300

    Refactor NextButton widget to use InkWell instead of ElevatedButton

--- lib/ui/features/intro/widgets/next_button.dart
+++ lib/ui/features/intro/widgets/next_button.dart
@@ -8,32 +8,27 @@ class NextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 79.44,
-      height: 80,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            left: 0,
-            bottom: 0,
-            child: Container(
-              width: 68,
-              height: 68,
-              clipBehavior: Clip.antiAlias,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(colors: <Color>[
-                  // #EF7E06, #F7B327
-                  Color(0xFFEF7E06),
-                  Color(0xFFF7B327),
-                ]),
-              ),
-              child: ElevatedButton(
-                onPressed: onPressed,
-                style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(),
-                  elevation: 0,
-                  backgroundColor: Colors.transparent,
+    return InkWell(
+      onTap: onPressed,
+      child: SizedBox(
+        width: 79.44,
+        height: 80,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 0,
+              bottom: 0,
+              child: Container(
+                width: 68,
+                height: 68,
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(colors: <Color>[
+                    // #EF7E06, #F7B327
+                    Color(0xFFEF7E06),
+                    Color(0xFFF7B327),
+                  ]),
                 ),
                 child: const Center(
                   child: Icon(
@@ -44,14 +39,14 @@ class NextButton extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-          const Positioned(
-              right: -1,
-              top: -1,
-              child: SizedBox(
-                child: ArcWidget(),
-              )),
-        ],
+            const Positioned(
+                right: -1,
+                top: -1,
+                child: SizedBox(
+                  child: ArcWidget(),
+                )),
+          ],
+        ),
       ),
     );
   }
