e9174eb2d4abb24365b39d378ab49bf16add40ad
Eren GUN <erengun4@gmail.com>
2023-12-08 00:58:44 +0300

    Add riverpod_extensions.dart for caching

--- lib/ui/home/logic/home_screen_logic.dart
+++ lib/ui/home/logic/home_screen_logic.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../data/network/paw_entry/paw_entry_repository.dart';
 import '../../../models/paw_entry.dart';
+import '../../../utils/riverpod_extensions.dart';
 import 'home_screen_ui_model.dart';
 
 part 'home_screen_logic.g.dart';
@@ -10,6 +11,8 @@ part 'home_screen_logic.g.dart';
 @riverpod
 Future<Either<String, GetPawEntryResponse>> fetchPawEntries(
     FetchPawEntriesRef ref) async {
+  /// OLMMM BU COK GUZEL BIR SEY
+  ref.cacheFor(const Duration(minutes: 10));
   final PawEntryRepository pawEntryRepository =
       ref.watch(getPawEntryRepositoryProvider);
   final Either<String, GetPawEntryResponse> pawEntries =
@@ -30,15 +33,13 @@ class HomeScreenLogic extends _$HomeScreenLogic {
         errorMessage: errorMessage,
         isLoading: false,
       );
-  
+
   void setPawEntries(List<PawEntry> pawEntries) => state = state.copyWith(
         pawEntries: pawEntries,
         isLoading: false,
       );
-  
+
   void setLoading() => state = state.copyWith(
         isLoading: true,
       );
-  
-  
 }
