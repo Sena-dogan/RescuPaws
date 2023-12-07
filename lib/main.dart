fcbe419bbf0a9f71a2917114de6f427aded46939
Eren GÜN <erengun4@gmail.com>
2023-12-07 21:19:41 +0300

    remove unused dependency on main

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ import 'package:stack_trace/stack_trace.dart' as stack_trace;
 
 import 'constants/strings.dart';
 import 'data/hive/hive.dart';
-import 'data/hive/hive_helper.dart';
 import 'di/components/service_locator.dart';
 import 'firebase_options.dart';
 import 'my_app.dart';
