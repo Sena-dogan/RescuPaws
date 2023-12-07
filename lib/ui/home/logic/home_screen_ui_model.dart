a102d9993b40e83288cf72b7e39a14f2b67f7849
Eren GÜN <erengun4@gmail.com>
2023-12-07 21:20:59 +0300

    implement home screen ui model

---
+++ lib/ui/home/logic/home_screen_ui_model.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../models/paw_entry.dart';
+
+part 'home_screen_ui_model.freezed.dart';
+
+@freezed
+class HomeScreenUiModel with _$HomeScreenUiModel {
+  factory HomeScreenUiModel({
+    @Default(<PawEntry>[]) List<PawEntry> pawEntries,
+    @Default(null) String? errorMessage,
+    @Default(false) bool isLoading,
+  }) = _HomeScreenUiModel;
+}
