b200a1ed695482123bdf639db9298d05f2e1635d
Eren GUN <erengun4@gmail.com>
2024-01-13 23:33:02 +0300

    Add NewPawModel conversion in NewPawUiModel

--- lib/ui/features/new_paw/model/new_paw_ui_model.dart
+++ lib/ui/features/new_paw/model/new_paw_ui_model.dart
@@ -4,10 +4,12 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:carousel_slider/carousel_controller.dart';
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:photo_manager/photo_manager.dart';
 
 import '../../../../models/location_response.dart';
+import '../../../../models/new_paw_model.dart';
 
 part 'new_paw_ui_model.freezed.dart';
 
@@ -18,7 +20,7 @@ abstract class NewPawUiModel with _$NewPawUiModel {
     @Default(null) String? error,
     @Default(0) int? page,
     @Default(<AssetEntity>[]) List<AssetEntity>? assets,
-    List<Uint8List>? imageBytes,
+    List<String>? imageBytes,
     CarouselController? carouselController,
     String? user_id,
     String? name,
@@ -32,7 +34,30 @@ abstract class NewPawUiModel with _$NewPawUiModel {
     String? age,
     String? weight,
     int? education,
-    bool? vaccinated,
+    @Default(0) int? vaccinated,
     String? address,
   }) = _NewPawUiModel;
+}
+
+// Extension of NewPawUiModel so that we can convert it to NewPawModel easily
+extension NewPawExtension on NewPawUiModel {
+  NewPawModel toNewPawModel() {
+    return NewPawModel(
+      address: address,
+      age: age,
+      /// Its sub_category_id not category_id in the backend sorry for the confusion
+      category_id: sub_category_id,
+      city_id: city?.id,
+      country_id: 1,
+      description: description,
+      district_id: district?.id,
+      education: education,
+      gender: gender,
+      name: name,
+      image: imageBytes,
+      user_id: user_id,
+      weight: weight,
+      vaccinated: vaccinated,
+    );
+  }
 }
