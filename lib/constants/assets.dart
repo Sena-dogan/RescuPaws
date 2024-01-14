dc00a899519baed94e5a560d49bd2c7599f87612
Eren GUN <erengun4@gmail.com>
2024-01-14 20:43:13 +0300

    Add loading animation asset

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -13,4 +13,5 @@ class Assets {
   static const String Success = 'assets/animations/success_animation.json';
   static const String Error = 'assets/animations/error_animation.json';
   static const String NotFound = 'assets/animations/404_animation.json'; 
+  static const String Loading = 'assets/animations/loading_animation.json';
 }
