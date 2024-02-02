3fe8df0949b26516b7995938c4f7e711f35e206c
Eren GÜN <erengun4@gmail.com>
2024-02-02 12:44:02 +0300

    Update base URL in endpoints.dart

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -1,6 +1,6 @@
 class Endpoints {
   Endpoints._();
-  static const String baseUrl = 'https://test.patipati.app/api/v1';
+  static const String baseUrl = 'https://cms.patipati.app/api/v1';
   static const int receiveTimeout = 15000;
   static const int connectionTimeout = 20000;
 
