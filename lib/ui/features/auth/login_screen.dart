8113f50a1d77c47c044934023ee11dd05d997ed9
Eren GÜN <erengun4@gmail.com>
2024-02-02 09:52:54 +0300

    Refactor login_screen ui

--- lib/ui/features/auth/login_screen.dart
+++ lib/ui/features/auth/login_screen.dart
@@ -1,18 +1,17 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:social_login_buttons/social_login_buttons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../config/router/app_router.dart';
 import '../../../constants/assets.dart';
 import '../../../utils/context_extensions.dart';
-import '../../widgets/app_bar_gone.dart';
 import 'login_logic.dart';
+import 'login_ui_model.dart';
+import 'social_button.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
@@ -22,8 +21,19 @@ class LoginScreen extends ConsumerStatefulWidget {
 }
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final LoginUiModel loginModel = ref.watch(loginLogicProvider);
     final Size size = MediaQuery.sizeOf(context);
     return Container(
       constraints: const BoxConstraints.expand(),
@@ -36,96 +46,292 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: const EmptyAppBar(),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Spacer(),
-              Image.asset(
-                Assets.PawPaw,
-                filterQuality: FilterQuality.none,
-                fit: BoxFit.none,
-              ),
-              Text(
-                'Giriş Yap',
-                style: context.textTheme.labelLarge?.copyWith(
-                  color: context.colorScheme.scrim,
-                ),
-              ),
-              const Spacer(),
-              _buildFooterButton(Size(size.width, size.height * 0.12)),
-              const Gap(10),
-            ],
+        appBar: AppBar(
+          backgroundColor: context.colorScheme.background,
+          shape: RoundedRectangleBorder(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(16),
+              bottomRight: Radius.circular(16),
+            ),
+            side: BorderSide(
+              color: context.colorScheme.scrim.withOpacity(0.2),
+            ),
+          ),
+          automaticallyImplyLeading: false,
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Kayıt Ol',
+                  style: context.textTheme.labelSmall?.copyWith(
+                      color: context.colorScheme.primary, fontSize: 16)),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: size.width,
+            height: size.height * 0.9,
+            child: Column(
+              children: <Widget>[
+                Gap(size.height * 0.05),
+                _loginText(context),
+                const Gap(25),
+                _buildLoginButtons(Size(size.width, size.height * 0.12)),
+                const Gap(25),
+                _buildOrDivider(size, context),
+                const Gap(25),
+                _emailText(context),
+                const Gap(25),
+                _buildEmail(size, context),
+                const Gap(16),
+                _buildPass(size, loginModel, context),
+                const Gap(16),
+                _buildTermsOfService(),
+                _buildPrivacyPolicy(),
+                const Gap(16),
+                _buildSignInButton(context),
+                _forgotPasswordButton(context),
+                const Spacer(),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Align _buildFooterButton(Size size) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        width: size.width * 0.9,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: SocialLoginButton(
-                  buttonType: SocialLoginButtonType.google,
-                  text: 'Google ile giriş yap',
-                  onPressed: () async {
-                    await ref
-                        .read(loginLogicProvider.notifier)
-                        .signInWithGoogle()
-                        .then((bool value) => value
-                            ? context.go(SGRoute.home.route)
-                            : context.showErrorSnackBar(
-                                title: 'Hata',
-                                message:
-                                    'Bir hata oluştu. Lütfen tekrar deneyiniz.'));
-                  },
-                  borderRadius: 30),
+  Text _emailText(BuildContext context) {
+    return Text('E-posta ile devam edin', style: context.textTheme.labelSmall);
+  }
+
+  Text _loginText(BuildContext context) {
+    return Text(
+      'Giriş Yap',
+      style: context.textTheme.labelSmall
+          ?.copyWith(color: context.colorScheme.scrim),
+    );
+  }
+
+  TextButton _forgotPasswordButton(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        context.go(SGRoute.forgotPassword.route);
+      },
+      child: Text(
+        'Şifremi unuttum',
+        style: context.textTheme.bodyMedium?.copyWith(
+          color: context.colorScheme.primary,
+        ),
+      ),
+    );
+  }
+
+  SizedBox _buildSignInButton(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.9,
+      height: 48,
+      child: ElevatedButton(
+        onPressed: () async {
+          await ref
+              .read(loginLogicProvider.notifier)
+              .signInWithEmailAndPassword()
+              .catchError((Object? err) {
+            debugPrint(err.toString());
+            context.showErrorSnackBar(
+                message: 'Bir hata oluştu. Lütfen tekrar deneyiniz.');
+            return false;
+          }).then((bool value) => value
+                  ? context.go(SGRoute.home.route)
+                  : context.showErrorSnackBar(
+                      message: 'Bir hata oluştu. Lütfen tekrar deneyiniz.'));
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: context.colorScheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+        ),
+        child: Text(
+          'Giriş Yap',
+          style: context.textTheme.bodyMedium!.copyWith(
+            color: context.colorScheme.background,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SizedBox _buildPass(
+      Size size, LoginUiModel loginModel, BuildContext context) {
+    return SizedBox(
+      width: size.width * 0.9,
+      height: 50,
+      child: TextFormField(
+        controller: _passwordController,
+        obscureText: loginModel.isObscure,
+        onChanged: (String value) {
+          ref.read(loginLogicProvider.notifier).passwordChanged(value);
+        },
+        autofillHints: const <String>[AutofillHints.password],
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.deny(RegExp(r'\s')),
+        ],
+        decoration: InputDecoration(
+          hintText: 'Şifre',
+          hintStyle: context.textTheme.bodyMedium?.copyWith(
+            color: context.colorScheme.scrim,
+          ),
+          fillColor: context.colorScheme.background,
+          filled: true,
+          contentPadding: const EdgeInsets.all(15),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: context.colorScheme.primary),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: context.colorScheme.primary),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          suffixIcon: IconButton(
+            onPressed: () {
+              ref.read(loginLogicProvider.notifier).toggleObscure();
+            },
+            icon: Icon(
+              loginModel.isObscure
+                  ? Icons.visibility_off_outlined
+                  : Icons.visibility_outlined,
+              color: context.colorScheme.scrim.withOpacity(0.5),
             ),
-            Visibility(
-              visible: Platform.isIOS,
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: SocialLoginButton(
-                    buttonType: SocialLoginButtonType.appleBlack,
-                    text: 'Apple ile giriş yap',
-                    onPressed: () {
-                      ref
-                          .read(loginLogicProvider.notifier)
-                          .signInWithApple()
-                          .then((bool value) => value
-                              ? context.go(SGRoute.home.route)
-                              : context.showErrorSnackBar(
-                                  title: 'Hata',
-                                  message:
-                                      'Bir hata oluştu. Lütfen tekrar deneyiniz.'));
-                    },
-                    borderRadius: 30),
+          ),
+        ),
+      ),
+    );
+  }
+
+  SizedBox _buildEmail(Size size, BuildContext context) {
+    return SizedBox(
+      width: size.width * 0.9,
+      height: 50,
+      child: TextFormField(
+          controller: _emailController,
+          onChanged: (String value) {
+            ref.read(loginLogicProvider.notifier).emailChanged(value);
+          },
+          autofillHints: const <String>[AutofillHints.email],
+          inputFormatters: <TextInputFormatter>[
+            FilteringTextInputFormatter.deny(RegExp(r'\s')),
+          ],
+          decoration: InputDecoration(
+            hintText: 'E-posta',
+            hintStyle: context.textTheme.bodyMedium?.copyWith(
+              color: context.colorScheme.scrim,
+            ),
+            fillColor: context.colorScheme.background,
+            filled: true,
+            contentPadding: const EdgeInsets.all(15),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: context.colorScheme.primary),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: context.colorScheme.primary),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: context.colorScheme.primary),
+              borderRadius: BorderRadius.circular(16),
+            ),
+          )),
+    );
+  }
+
+  SizedBox _buildOrDivider(Size size, BuildContext context) {
+    return SizedBox(
+      width: size.width * 0.9,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Divider(
+              color: context.colorScheme.scrim.withOpacity(0.2),
+              thickness: 1,
+            ),
+          ),
+          const Gap(10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text(
+              'veya',
+              style: context.textTheme.bodyMedium?.copyWith(
+                color: context.colorScheme.scrim,
               ),
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: SocialLoginButton(
-                  buttonType: SocialLoginButtonType.generalLogin,
-                  text: 'E-posta ile giriş yap',
-                  onPressed: () {
-                    context.push(SGRoute.emailLogin.route);
-                  },
-                  backgroundColor: context.colorScheme.primary,
-                  borderRadius: 30),
+          ),
+          const Gap(10),
+          Expanded(
+            child: Divider(
+              color: context.colorScheme.scrim.withOpacity(0.2),
+              thickness: 1,
             ),
-            _buildTermsOfService(),
-            _buildPrivacyPolicy(),
-          ],
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoginButtons(Size size) {
+    return Container(
+      color: Colors.transparent,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      width: size.width * 0.9,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            color: Colors.transparent,
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: SocialLoginButton(
+                elevation: const MaterialStatePropertyAll<double>(0.0),
+                strokeColor: context.colorScheme.primary,
+                buttonType: SocialLoginButtonType.google,
+                backgroundColor: Colors.transparent,
+                text: 'Google ile devam et',
+                textColor: context.colorScheme.scrim,
+                onPressed: () async {
+                  await ref
+                      .read(loginLogicProvider.notifier)
+                      .signInWithGoogle()
+                      .then((bool value) => value
+                          ? context.go(SGRoute.home.route)
+                          : context.showErrorSnackBar(
+                              message:
+                                  'Bir hata oluştu. Lütfen tekrar deneyiniz.'));
+                },
+                borderRadius: 30),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: SocialLoginButton(
+                elevation: const MaterialStatePropertyAll<double>(0.0),
+                strokeColor: context.colorScheme.primary,
+                backgroundColor: Colors.transparent,
+                textColor: context.colorScheme.scrim,
+                buttonType: SocialLoginButtonType.apple,
+                text: 'Apple ile devam et',
+                onPressed: () {
+                  ref.read(loginLogicProvider.notifier).signInWithApple().then(
+                      (bool value) => value
+                          ? context.go(SGRoute.home.route)
+                          : context.showErrorSnackBar(
+                              message:
+                                  'Bir hata oluştu. Lütfen tekrar deneyiniz.'));
+                },
+                borderRadius: 30),
+          ),
+        ],
       ),
     );
   }
@@ -136,14 +342,6 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-            'onaylamış olursunuz.',
-            style: GoogleFonts.outfit(
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-              color: context.colorScheme.scrim,
-            ),
-          ),
           GestureDetector(
             onTap: () {
               const String url = 'https://patipati.app/privacy-policy';
@@ -157,9 +355,16 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
               style: GoogleFonts.outfit(
                 fontSize: 14,
                 fontWeight: FontWeight.w400,
-                decoration: TextDecoration.underline,
-                color: context.colorScheme.scrim,
+                color: context.colorScheme.primary,
               ),
+            ),
+          ),
+          Text(
+            'onaylamış olursunuz.',
+            style: GoogleFonts.outfit(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: context.colorScheme.scrim,
             ),
           ),
         ],
@@ -196,8 +401,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                 style: GoogleFonts.outfit(
                   fontSize: 14,
                   fontWeight: FontWeight.w400,
-                  decoration: TextDecoration.underline,
-                  color: context.colorScheme.scrim,
+                  color: context.colorScheme.primary,
                 ),
               ),
             ),
