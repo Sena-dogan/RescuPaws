874c1bbb267caa28d4c3d98832bada92342f9c79
Eren GÜN <erengun4@gmail.com>
2024-02-02 15:34:56 +0300

    Refactor bottom navigation bar widget

--- lib/ui/widgets/bottom_nav_bar.dart
+++ lib/ui/widgets/bottom_nav_bar.dart
@@ -16,33 +16,73 @@ class BottomNavBar extends ConsumerWidget {
     // ignore: prefer_final_locals, always_specify_types
     var nav = ref.watch(bottomNavBarLogicProvider);
 
-    return AnimatedBottomNavigationBar(
-      icons: const <IoniconsData>[
-        Ionicons.home_outline,
-        Ionicons.person_outline,
-      ],
-      activeIndex: nav.navIndex,
-      onTap: (int index) {
-        ref.read(bottomNavBarLogicProvider.notifier).setNavIndex(index);
-        debugPrint('Current Index is $index');
-        debugPrint('Current Route is ${SGRoute.values[index].route}');
-        context.go(SGRoute.values[index].route);
-      },
-      gapLocation: GapLocation.center,
-      //borderColor: Colors.grey.withOpacity(0.5),
-      notchSmoothness: NotchSmoothness.softEdge,
-      leftCornerRadius: 20,
-      rightCornerRadius: 20,
-      splashColor: context.colorScheme.primary,
-      inactiveColor: context.colorScheme.tertiary,
-      activeColor: context.colorScheme.primary,
-      iconSize: 24,
-      height: 70,
-      backgroundColor: context.colorScheme.tertiaryContainer,
-      shadow: Shadow(
-        color: Colors.grey.withOpacity(0.2),
-        blurRadius: 10,
-      )
-    );
+    return AnimatedBottomNavigationBar.builder(
+        itemCount: 2,
+        tabBuilder: (int index, bool isActive) {
+          return index == 0
+              ? Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Icon(
+                        Ionicons.home_outline,
+                        color: isActive
+                            ? context.colorScheme.primary
+                            : context.colorScheme.tertiary,
+                        size: 24,
+                      ),
+                    ),
+                    Text(
+                      'Ana Sayfa',
+                      style: TextStyle(
+                        color: isActive
+                            ? context.colorScheme.primary
+                            : context.colorScheme.tertiary,
+                      ),
+                    ),
+                  ],
+                )
+              : Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Icon(
+                        Ionicons.person_outline,
+                        color: isActive
+                            ? context.colorScheme.primary
+                            : context.colorScheme.tertiary,
+                        size: 24,
+                      ),
+                    ),
+                    Text(
+                      'Profil',
+                      style: TextStyle(
+                        color: isActive
+                            ? context.colorScheme.primary
+                            : context.colorScheme.tertiary,
+                      ),
+                    ),
+                  ],
+                );
+        },
+        activeIndex: nav.navIndex,
+        onTap: (int index) {
+          ref.read(bottomNavBarLogicProvider.notifier).setNavIndex(index);
+          debugPrint('Current Index is $index');
+          debugPrint('Current Route is ${SGRoute.values[index].route}');
+          context.go(SGRoute.values[index].route);
+        },
+        gapLocation: GapLocation.center,
+        //borderColor: Colors.grey.withOpacity(0.5),
+        notchSmoothness: NotchSmoothness.softEdge,
+        leftCornerRadius: 20,
+        rightCornerRadius: 20,
+        splashColor: context.colorScheme.primary,
+        height: 70,
+        backgroundColor: context.colorScheme.tertiaryContainer,
+        shadow: Shadow(
+          color: Colors.grey.withOpacity(0.2),
+          blurRadius: 10,
+        ));
   }
 }
