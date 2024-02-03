8602600e0563f8dbbfc0b61b2f769e5253b8b732
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2024-02-03 16:18:00 +0300

    Add vaccine details to PawEntryDetail and update VaccineScreen UI

--- lib/ui/features/detail/vaccine_page.dart
+++ lib/ui/features/detail/vaccine_page.dart
@@ -23,10 +23,18 @@ class VaccineScreen extends ConsumerWidget {
           backgroundColor: Colors.transparent,
           appBar: AppBar(
             leading: IconButton(
-              icon: Icon(
-                Icons.arrow_back_ios_new_rounded,
-                size: 30,
-                color: context.colorScheme.scrim,
+              icon: Container(
+                height: 50,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.arrow_back,
+                  size: 20,
+                  color: Colors.white,
+                ),
               ),
               onPressed: () {
                 context.pop();
@@ -39,6 +47,94 @@ class VaccineScreen extends ConsumerWidget {
               style: context.textTheme.labelSmall,
             ),
           ),
+          body: const SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    HaveVaccineWidget(
+                      title: 'Rabies Aşısı',
+                      haveVaccine: true,
+                    ),
+                    HaveVaccineWidget(
+                      title: 'Distemper Aşısı',
+                      haveVaccine: false,
+                    ),
+                    HaveVaccineWidget(
+                      title: 'Hepatitis Aşısı',
+                      haveVaccine: false,
+                    ),
+                    HaveVaccineWidget(
+                      title: 'Parvovirus Aşısı',
+                      haveVaccine: true,
+                    ),
+                    HaveVaccineWidget(
+                      title: 'Bordotella Aşısı',
+                      haveVaccine: true,
+                    ),
+                    HaveVaccineWidget(
+                      title: 'Leptospirosis Aşısı',
+                      haveVaccine: false,
+                    ),
+                    HaveVaccineWidget(
+                      title: 'Panleukopenia Aşısı',
+                      haveVaccine: true,
+                    ),
+                    HaveVaccineWidget(
+                      title: 'Herpesvirus and Calicivirus Aşısı',
+                      haveVaccine: false,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ));
+  }
+}
+
+class HaveVaccineWidget extends StatelessWidget {
+  const HaveVaccineWidget({
+    required this.title,
+    required this.haveVaccine,
+    super.key,
+  });
+
+  final String title;
+  final bool haveVaccine;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.sizeOf(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        width: size.width * 0.9,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: haveVaccine
+                ? Colors.green.withOpacity(0.4)
+                : Colors.red.withOpacity(0.4),
+            width: 2,
+          ),
+        ),
+        child: ListTile(
+          title: Text(
+            title,
+            style: context.textTheme.bodyLarge,
+            selectionColor: context.colorScheme.scrim,
+          ),
+          trailing: haveVaccine
+              ? Icon(Icons.done_outline_rounded,
+                  color: Colors.green.withOpacity(0.8), size: 30)
+              : Icon(Icons.close_rounded,
+                  color: Colors.red.withOpacity(0.8), size: 30),
+        ),
+      ),
+    );
   }
 }
