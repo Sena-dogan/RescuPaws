19efbd7faab12bac863f64168fb958b562f95186
Eren GÜN <erengun4@gmail.com>
2024-02-09 10:06:01 +0300

    Add CreateFavoriteRequest and CreateFavoriteResponse models

---
+++ lib/models/favorite/create_favorite_request.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'create_favorite_request.freezed.dart';
+part 'create_favorite_request.g.dart';
+
+@freezed
+class CreateFavoriteRequest with _$CreateFavoriteRequest {
+  const factory CreateFavoriteRequest({
+    required String? uid,
+    required int class_field_id,
+    /// 1 for favorite, 0 for unfavorite
+    required int is_favorite,
+  }) = _CreateFavoriteRequest;
+
+  factory CreateFavoriteRequest.fromJson(Map<String, dynamic> json) =>
+      _$CreateFavoriteRequestFromJson(json);
+}
---
+++ lib/models/favorite/create_favorite_response.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'create_favorite_response.freezed.dart';
+part 'create_favorite_response.g.dart';
+
+@freezed
+class CreateFavoriteResponse with _$CreateFavoriteResponse {
+  const factory CreateFavoriteResponse({
+    required String? status,
+    required String? message,
+  }) = _CreateFavoriteResponse;
+
+  factory CreateFavoriteResponse.fromJson(Map<String, dynamic> json) =>
+      _$CreateFavoriteResponseFromJson(json);
+}
