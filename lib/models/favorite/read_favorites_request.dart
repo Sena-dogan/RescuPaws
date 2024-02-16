4de6ab8bf2065368520d12b6e6777d68d0b411b5
Eren GÜN <erengun4@gmail.com>
2024-02-16 09:40:01 +0300

    Add ReadFavoritesRequest model

---
+++ lib/models/favorite/read_favorites_request.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'read_favorites_request.freezed.dart';
+part 'read_favorites_request.g.dart';
+
+@freezed
+class ReadFavoritesRequest with _$ReadFavoritesRequest {
+  const factory ReadFavoritesRequest({
+    required String? uid,
+  }) = _ReadFavoritesRequest;
+
+  factory ReadFavoritesRequest.fromJson(Map<String, dynamic> json) =>
+      _$ReadFavoritesRequestFromJson(json);
+}
