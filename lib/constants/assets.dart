157dc6ec84e9ab21fbf45420f8363c7e9d9d6f63
Eren GÜN <erengun4@gmail.com>
2024-02-17 23:14:11 +0300

    Add Favorites background image

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -2,6 +2,7 @@ class Assets {
   Assets._();
   static const String LoginBg = 'assets/images/login_bg.png';
   static const String HomeBg = 'assets/images/home_bg.png';
+  static const String FavoritesBg = 'assets/images/favorites_bg.png';
   static const String PawPaw = 'assets/images/pawpaw.png';
   static const String PatiApp = 'assets/images/pati_app.png';
   static const String Arc = 'assets/images/arc.png';
