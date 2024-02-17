e24e683f1c6f436c2ca6cf2c3b0481581809340c
Eren GÜN <erengun4@gmail.com>
2024-02-17 22:02:16 +0300

    Add FavoriteUiModel class

---
+++ lib/ui/features/favorite/logic/favorite_ui_model.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../../models/favorite/favorite_model.dart';
+
+part 'favorite_ui_model.freezed.dart';
+
+@freezed
+class FavoriteUiModel with _$FavoriteUiModel {
+  const factory FavoriteUiModel({
+    @Default(false) bool isLoading,
+    @Default('') String errorMessage,
+    @Default(<Favorite>[]) List<Favorite> favoriteList,
+  }) = _FavoriteUiModel;
+}
