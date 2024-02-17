21040f4288f3b236b5b3206401fb25e24e284ef0
Eren GÜN <erengun4@gmail.com>
2024-02-17 23:14:25 +0300

    Add deleteFavorite function to FavoriteLogic

--- lib/ui/features/favorite/logic/favorite_logic.dart
+++ lib/ui/features/favorite/logic/favorite_logic.dart
@@ -1,7 +1,10 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../../data/network/favorite/favorite_repository.dart';
+import '../../../../models/favorite/delete_favorite_response.dart';
+import '../../../../models/favorite/delete_favorites_request.dart';
 import '../../../../models/favorite/favorite_model.dart';
+import '../../../../utils/firebase_utils.dart';
 import '../../../../utils/riverpod_extensions.dart';
 import 'favorite_ui_model.dart';
 
@@ -12,15 +15,30 @@ Future<List<Favorite>?> fetchFavoriteList(FetchFavoriteListRef ref) async {
   final FavoriteRepository favoriteRepository =
       ref.watch(getFavoriteRepositoryProvider);
   ref.cacheFor(const Duration(minutes: 5));
-  final List<Favorite> favoriteList = await favoriteRepository.getFavoriteList();
+  final List<Favorite> favoriteList =
+      await favoriteRepository.getFavoriteList();
   return favoriteList;
-} 
+}
+
+@riverpod
+Future<DeleteFavoriteResponse> deleteFavorite(
+    DeleteFavoriteRef ref, Favorite favorite) async {
+  final FavoriteRepository favoriteRepository =
+      ref.watch(getFavoriteRepositoryProvider);
+  final DeleteFavoriteRequest deleteFavoritesRequest = DeleteFavoriteRequest(
+    class_field_id: favorite.classfield!.id,
+    uid: currentUserUid,
+  );
+  final DeleteFavoriteResponse deleteFavoriteResponse =
+      await favoriteRepository.deleteFavorite(deleteFavoritesRequest);
+  return deleteFavoriteResponse;
+}
 
 @riverpod
 class FavoriteLogic extends _$FavoriteLogic {
   @override
   FavoriteUiModel build() {
-    return FavoriteUiModel();
+    return const FavoriteUiModel();
   }
 
   void setError(String errorMessage) => state = state.copyWith(
@@ -28,13 +46,31 @@ class FavoriteLogic extends _$FavoriteLogic {
         isLoading: false,
       );
 
-  void setFavoriteList(List<Favorite> favoriteList) =>
-      state = state.copyWith(
+  void setFavoriteList(List<Favorite> favoriteList) => state = state.copyWith(
         favoriteList: favoriteList,
         isLoading: false,
       );
 
-  void setLoading() => state = state.copyWith(
-        isLoading: true,
+  void setLoading({bool isLoading = true}) => state = state.copyWith(
+        isLoading: isLoading,
       );
+
+  Future<void> deleteFavorite(Favorite favorite) async {
+    setLoading();
+    final AsyncValue<DeleteFavoriteResponse> deleteFavoriteResponse =
+        ref.read(deleteFavoriteProvider(favorite));
+    deleteFavoriteResponse.whenData(
+      (DeleteFavoriteResponse value) {
+        if (value.status == 'success') {
+          final List<Favorite> updatedFavoriteList = state.favoriteList
+              .where((Favorite element) =>
+                  element.classfield!.id != favorite.classfield!.id)
+              .toList();
+          setFavoriteList(updatedFavoriteList);
+        } else {
+          setError(value.message ?? 'An error occurred');
+        }
+      },
+    );
+  }
 }
