3ba4ff897641cd41b246c0d3646e127a0462288e
Eren GÜN <erengun4@gmail.com>
2024-02-24 23:46:49 +0300

    Update timeout values in endpoints.dart

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -1,8 +1,8 @@
 class Endpoints {
   Endpoints._();
   static const String baseUrl = 'https://test.patipati.app/api/v1';
-  static const int receiveTimeout = 15000;
-  static const int connectionTimeout = 20000;
+  static const int receiveTimeout = 30000;
+  static const int connectionTimeout = 30000;
 
   // booking endpoints
 }
