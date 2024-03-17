3d2fd8fc1d2f29dde89167392328613a5c86adb1
Eren GÜN <erengun4@gmail.com>
2024-03-17 12:43:16 +0300

    Refactor number input screen UI and add phone number verification

--- lib/ui/features/auth/number_input_screen.dart
+++ lib/ui/features/auth/number_input_screen.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
@@ -36,57 +39,82 @@ class _NumberInputScreenState extends ConsumerState<NumberInputScreen> {
       child: Scaffold(
         appBar: const EmptyAppBar(),
         backgroundColor: Colors.transparent,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('SMS Doğrulama', style: context.textTheme.titleLarge),
-            const Gap(20),
-            Text(
-              'Sevimli dostlarımızla tanışmak için telefonuna bir doğrulama kodu göndereceğiz.',
-              style: context.textTheme.bodyMedium,
-            ),
-            const Gap(20),
-            Form(
-              key: formKey,
-              child: TextFormField(
-                controller: numberController,
-                validator: (String? value) {
-                  if (value!.isEmpty) {
-                    return 'Lütfen telefon numaranızı girin';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: context.colorScheme.background,
-                  hintText: '+90__   __.  __.  ___',
-                  hintStyle: context.textTheme.bodyMedium,
-                  labelStyle: context.textTheme.bodyMedium,
-                  border: _mainBorder(),
-                  focusedBorder: _mainBorder(),
-                  enabledBorder: _mainBorder(),
-                  errorBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(
-                      color: context.colorScheme.error,
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Spacer(),
+              Align(
+                  child: Text('SMS Doğrulama',
+                      style: context.textTheme.titleLarge)),
+              const Gap(20),
+              Text(
+                'Sevimli dostlarımızla tanışmak için telefonuna bir doğrulama kodu göndereceğiz.',
+                style: context.textTheme.bodyMedium,
+              ),
+              const Gap(20),
+              Form(
+                key: formKey,
+                child: TextFormField(
+                  controller: numberController,
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Lütfen telefon numaranızı giriniz';
+                    } else {
+                      FirebaseAuth.instance.verifyPhoneNumber(
+                        phoneNumber: value,
+                        autoRetrievedSmsCodeForTesting: '123456',
+                        forceResendingToken: 1,
+                        verificationCompleted:
+                            (PhoneAuthCredential credential) {
+                          debugPrint('verificationCompleted');
+                        },
+                        verificationFailed: (FirebaseAuthException e) {
+                          debugPrint('verificationFailed');
+                        },
+                        codeSent: (String verificationId, int? resendToken) {
+                          debugPrint('codeSent');
+                        },
+                        codeAutoRetrievalTimeout: (String verificationId) {
+                          debugPrint('codeAutoRetrievalTimeout');
+                        },
+                      );
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: context.colorScheme.background,
+                    hintText: '+90__   __.  __.  ___',
+                    hintStyle: context.textTheme.bodyMedium,
+                    labelStyle: context.textTheme.bodyMedium,
+                    border: _mainBorder(),
+                    focusedBorder: _mainBorder(),
+                    enabledBorder: _mainBorder(),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(
+                        color: context.colorScheme.error,
+                      ),
                     ),
                   ),
+                  keyboardType: TextInputType.phone,
                 ),
-                keyboardType: TextInputType.phone,
               ),
-            ),
-            Align(
-              alignment: AlignmentDirectional.centerEnd,
-              child: SaveButton(
+              const Spacer(
+                flex: 2,
+              ),
+              SaveButton(
                   onPressed: () {
                     if (formKey.currentState?.validate() ?? false) {
                       debugPrint('Devam Et');
                     }
                   },
                   title: 'Devam Et'),
-            ),
-          ],
+              const Spacer(),
+            ],
+          ),
         ),
       ),
     );
