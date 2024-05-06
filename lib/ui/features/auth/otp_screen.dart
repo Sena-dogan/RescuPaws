54a1a685cc0f71eb78ed321294ed3acf277b76f9
Eren GÜN <erengun4@gmail.com>
2024-05-06 10:56:30 +0300

    Refactor OTP screen to use Pinput for more intuitive SMS code input
    The code changes replace the existing OTPTextField with Pinput to provide a more user-friendly and intuitive SMS code input experience. This improves the usability and enhances the overall functionality of the OTP screen. No associated issues are referenced at this time.

--- lib/ui/features/auth/otp_screen.dart
+++ lib/ui/features/auth/otp_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
-import 'package:otp_text_field/otp_field.dart';
-import 'package:otp_text_field/style.dart';
+import 'package:logger/logger.dart';
 import 'package:otp_timer_button/otp_timer_button.dart';
+import 'package:pinput/pinput.dart';
 
 import '../../../config/router/app_router.dart';
 import '../../../constants/assets.dart';
@@ -30,41 +31,60 @@ class OtpScreen extends ConsumerWidget {
           appBar: const EmptyAppBar(),
           backgroundColor: Colors.transparent,
           body: Padding(
-            padding: const EdgeInsets.all(12.0),
+            padding: const EdgeInsets.all(20.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const Spacer(),
                 Align(
                     child: Text('SMS Doğrulama',
                         style: context.textTheme.titleLarge)),
                 const Gap(20),
-                Text(
-                  'Cep telefonuna tek seferlik bir kod gönderdik.',
-                  style: context.textTheme.bodyMedium,
+                Align(
+                  child: Text(
+                    'Cep telefonuna tek seferlik bir kod gönderdik.',
+                    style: context.textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
                 ),
                 const Gap(20),
-                OTPTextField(
-                  length: 6,
-                  width: MediaQuery.sizeOf(context).width,
-                  fieldWidth: 50,
-                  style: const TextStyle(fontSize: 17),
-                  textFieldAlignment: MainAxisAlignment.spaceAround,
-                  fieldStyle: FieldStyle.box,
-                  onCompleted: (String pin) {
-                    ref
-                        .read(loginLogicProvider.notifier)
-                        .verifySmsCode(pin)
-                        .then((bool value) {
-                      if (value) {
-                        context.showAwesomeMaterialBanner(
-                          title: 'Başarılı',
-                          message: 'Sms doğrulama başarılı',
-                        );
-                        context.go(SGRoute.home.route);
-                      }
-                    });
-                  },
+                Align(
+                  child: Pinput(
+                    length: 6,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    controller: ref.watch(loginLogicProvider).otpController,
+                    onCompleted: (String value) {
+                      ref
+                          .read(loginLogicProvider.notifier)
+                          .verifySmsCode(value)
+                          .then((bool value) {
+                        if (value) {
+                          context.showAwesomeMaterialBanner(
+                            title: 'Başarılı',
+                            message: 'Sms doğrulama başarılı',
+                          );
+                          context.go(SGRoute.home.route);
+                        }
+                      });
+                    },
+
+                    
+                    defaultPinTheme: PinTheme(
+                      width: 50,
+                      height: 50,
+                      constraints: BoxConstraints(
+                        minHeight: 50,
+                        maxWidth: context.width,
+                      ),
+                        decoration: BoxDecoration(
+                      color: context.colorScheme.background,
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(color: context.colorScheme.primary),
+                    )),
+                  ),
                 ),
                 const Gap(20),
                 Center(
@@ -88,48 +108,20 @@ class OtpScreen extends ConsumerWidget {
                             .resendSmsCode()
                             .then((bool value) {
                           if (value) {
-                            context.showAwesomeMaterialBanner(
-                              title: 'Başarılı',
-                              message: 'Sms tekrar gönderildi',
-                            );
+                            Logger().i('Otp Screen: Sms tekrar gönderildi');
                           }
                         });
                       },
                     ),
                   ],
                 )
-                    // child: RichText(
-                    //     text: TextSpan(
-                    //   text: 'Kod eline ulaşmadı mı? ',
-                    //   style: context.textTheme.bodySmall,
-                    //   children: <InlineSpan>[
-                    //     TextSpan(
-                    //       recognizer: TapGestureRecognizer()
-                    //         ..onTap = () {
-                    //           ref
-                    //               .read(loginLogicProvider.notifier)
-                    //               .resendSmsCode()
-                    //               .then((bool value) {
-                    //             if (value) {
-                    //               context.showAwesomeMaterialBanner(
-                    //                 title: 'Başarılı',
-                    //                 message: 'Sms tekrar gönderildi',
-                    //               );
-                    //             }
-                    //           });
-                    //         },
-                    //       text: 'Yeniden gönder.',
-                    //       style: context.textTheme.bodySmall?.copyWith(
-                    //         color: context.colorScheme.primary,
-                    //       ),
-                    //     ),
-                    //   ],
-                    // )),
                     ),
                 const Spacer(
                   flex: 2,
                 ),
-                SaveButton(onPressed: () {}, title: 'Devam Et'),
+                SaveButton(onPressed: () {
+                  context.go(SGRoute.home.route);
+                }, title: 'Doğrula'),
                 const Spacer(),
               ],
             ),
