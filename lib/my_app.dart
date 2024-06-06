671a88a70309122d09c4fa2bfd04431c6cd7619a
Eren GÜN <erengun4@gmail.com>
2024-06-06 13:40:40 +0300

    feat: Add GoRouter to MaterialApp.router configuration

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -3,11 +3,11 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'config/router/app_router.dart';
 import 'config/theme/theme_logic.dart';
 import 'config/theme/theme_ui_model.dart';
-import 'di/components/service_locator.dart';
 import 'utils/pati_color_scheme.dart';
 
 class MyApp extends ConsumerWidget {
@@ -16,8 +16,9 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final ThemeUiModel currentTheme = ref.watch(themeLogicProvider);
+    final GoRouter goRouter = ref.watch(goRouteProvider);
     return MaterialApp.router(
-      routerConfig: getIt<SGGoRouter>().getGoRouter,
+      routerConfig: goRouter,
 
       /// Localization is not available for the title.
       title: 'Pati Pati App',
