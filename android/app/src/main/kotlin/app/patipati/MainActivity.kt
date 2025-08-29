daf80b1d6495911b7bbd7161355c9dfee19461f0
Eren Gun <erengun4@gmail.com>
2025-08-29 12:11:48 +0300

    feat: Update iOS project dependencies and assets
    - Added new Swift Package Manager dependencies in Package.resolved.
    - Modified AppDelegate.swift to import UIKit.
    - Updated app icons in Assets.xcassets with new images.
    - Removed unused launch background images and updated LaunchScreen.storyboard.
    - Changed app display name in Info.plist from "Pati Pati" to "RescuPaws".
    - Deleted Fastlane configuration files and reports as they are no longer needed.

--- android/app/src/main/kotlin/app/patipati/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package app.patipati
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/io/devopen/rescupaws/rescupaws/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.devopen.rescupaws
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
